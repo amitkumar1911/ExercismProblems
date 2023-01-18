6797edd5c7be0770fcc040a8d2606d3706364c38
amitkumar1911 <amit.khan@zopsmart.com>
2023-01-18 09:15:59 +0530

    lasagna master

---
+++ lasagna/lasagna.go
@@ -0,0 +1,31 @@
+package lasagna
+
+// import "fmt"
+
+// TODO: define the 'OvenTime' constant
+const OvenTime=40
+
+// fmt.Println(ovenTime)
+
+
+// RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
+func RemainingOvenTime(actualMinutesInOven int) int {
+     // ovenTime=40;
+	// panic("RemainingOvenTime not implemented")
+
+    return OvenTime-actualMinutesInOven;
+}
+
+// PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func PreparationTime(numberOfLayers int) int {
+	// panic("PreparationTime not implemented")
+    return 2*numberOfLayers;
+}
+
+// ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	// panic("ElapsedTime not implemented")
+
+    return PreparationTime(numberOfLayers) + actualMinutesInOven
+    
+}
